d80ed58e084cf6df81998817367f2ad8ef2a9179
Eric Dean Kauffman <eric@moonrise.net>
2023-03-12 15:54:29 -0700

    Add project files.

---
+++ Apex.Redirector/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Apex.Redirector.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Apex.Redirector.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            switch (this.Request.Host.Host.ToLower())
+            {
+                case "moonrise.net":
+                    return RedirectPermanent("https://www.moonrise.net");
+                case "cosmoswps.com":
+                    return RedirectPermanent("https://www.cosmoswps.com");
+
+            }
+            return NotFound(404.100);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
